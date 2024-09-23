cc0a6f64120444d56fcd4b23ad4a4cfb48ffe2fb
reshmag <reshmag@localhost>
2024-09-23 07:49:45 +0530

    first commit

---
+++ My_Maven/src/main/java/exception/Sample.java
@@ -0,0 +1,29 @@
+package exception;
+
+public class Sample {
+	public void test(int age) throws LicenseException//declare
+	{
+		
+			if(age<18)
+			{
+				
+			
+			throw new LicenseException("not elligible");//throwan exception
+			//System.out.println("Not elligible for voting");
+		}
+			else
+			{
+				System.out.println("Elligible for Voting");
+			}
+		}
+	
+
+	public static void main(String[] args) throws LicenseException {
+		Sample obj = new Sample();
+		obj.test(60);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ My_Maven/src/main/java/inheritance/Parent.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Parent extends GrandParent  {
+	public    void sub()
+
+	{
+		System.out.println(a-b);
+	}
+	
+
+}
