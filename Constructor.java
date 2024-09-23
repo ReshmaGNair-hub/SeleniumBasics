6cd35c1fccac040c9ced2fa6801814a78970f4fe
ReshmaGNair-hub <reshmavijayabhanu111@gmail.com>
2024-09-23 07:21:03 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,15 @@
+class Constructor
+{
+char a = 'H';
+ static String b = "Hi";
+public Constructor()
+{
+System.out.println(b);
+System.out.println(a);
+}
+public static void main(String args[])
+{
+Constructor obj= new Constructor();
+}
+}
+
